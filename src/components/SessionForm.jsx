dcf561afd489963a5d4c6545f9e3fa36beced122
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-12-21 20:15:41 -0330

    added .session-form and .form-section

--- src/components/SessionForm.jsx
+++ src/components/SessionForm.jsx
@@ -9,16 +9,19 @@ const SessionForm = ({ onAddSession }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="header">Session Title</label>
-      <input type="text" id="header" {...register('header', { required: true })} />
-
-      <label htmlFor="body">Body:</label>
-      <textarea id="body" {...register('body', { required: true })} />
-
-      <label htmlFor="date">Date:</label>
-      <input type="date" id="date" {...register('date', { required: true })} />
-
+    <form onSubmit={handleSubmit(onSubmit)} className="session-form">
+      <div className="form-section">
+        <label htmlFor="header">Session Title</label>
+        <input type="text" id="header" {...register('header', { required: true })} />
+      </div>
+      <div className="form-section">
+        <label htmlFor="body">Body:</label>
+        <textarea id="body" {...register('body', { required: true })} />
+      </div>
+      <div className="form-section">
+        <label htmlFor="date">Date:</label>
+        <input type="date" id="date" {...register('date', { required: true })} />
+      </div>
       <button type="submit">SUBMIT</button>
     </form>
   );
