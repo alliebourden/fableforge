426d5259646c922eed632211dc174120bf6790d4
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-12-23 10:35:03 -0330

    routed SessionEditor

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import SessionEditor from "./pages/SessionEditor";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashboard />,
+  },
+  {
+    path: "/session-editor",
+    element: <SessionEditor />,
   },
 ]);
 
---
+++ src/pages/SessionEditor.jsx
@@ -0,0 +1,9 @@
+export default function SessionEditor() {
+  return (
+    <div>
+      <header>
+        <span>SESSION</span>
+      </header>
+    </div>
+  );
+}
