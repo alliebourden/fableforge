cc070a92ee0b17befffb19d3cbffa5d7d511f520
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-12-23 11:31:14 -0330

    added logo to top-bar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,13 @@
-// put in header - own component
-// put in side panel - own component
-// style in layout
 import router from "../Routes";
-import logo from ".assets/images/logo-small.png";
 import { RouterProvider } from "react-router-dom";
+import Logo from "./images/logo-small.png";
 
 export default function Layout() {
   return (
     <div>
       <RouterProvider router={router} />
       <div className="top-bar">
-        <img src={logo}></img>
+        <img src={Logo}></img>
       </div>
     </div>
   );
