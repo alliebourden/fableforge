0fb7ae05c2ed7a121186c3a106190f62248b3d60
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-12-23 11:06:49 -0330

    uploaded logo

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,6 +2,7 @@
 // put in side panel - own component
 // style in layout
 import router from "../Routes";
+import logo from ".assets/images/logo-small.png";
 import { RouterProvider } from "react-router-dom";
 
 export default function Layout() {
@@ -9,7 +10,7 @@ export default function Layout() {
     <div>
       <RouterProvider router={router} />
       <div className="top-bar">
-        <img></img>
+        <img src={logo}></img>
       </div>
     </div>
   );
