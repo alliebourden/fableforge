017d68cbaaad2689f244b3199ef453497b5d06df
alliebourden <127435154+alliebourden@users.noreply.github.com>
2024-01-05 15:47:43 -0330

    chore - created CategoryTags const

--- src/components/CategoryTags.jsx
+++ src/components/CategoryTags.jsx
@@ -1,2 +1,11 @@
 import React from "react";
 import Select from "react-select'";
+
+const CategoryTags = [
+  { value: "combat", label: "Combat" },
+  { value: "shopping", label: "Shopping" },
+  { value: "new quest", label: "New Quest" },
+  { value: "roleplay", label: "Roleplay" },
+  { value: "pc backstory", label: "PC Backstory" },
+  { value: "npc", label: "NPC" },
+];
