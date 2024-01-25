82332054f36b9493622add0d126d813da71eac6f
alliebourden <127435154+alliebourden@users.noreply.github.com>
2024-01-25 19:13:57 -0330

    chore - minor formatting changes

--- src/components/SessionList.jsx
+++ src/components/SessionList.jsx
@@ -16,15 +16,24 @@ const SessionList = () => {
       <div className="recent-session">
         <div className="last-session-top">
           <p>Last Session</p>
+          <div className="last-session-top-date">
+            {sessions[sessions.length - 1].date}{" "}
+          </div>
         </div>
         {sessions.length > 0 && (
           <div className="last-session-body">
-            <strong>{sessions[sessions.length - 1].header}</strong> -{" "}
-            {sessions[sessions.length - 1].date}
-            <p>{sessions[sessions.length - 1].body}</p>
-            {sessions[sessions.length - 1].tags && (
-              <p>Tags: {sessions[sessions.length - 1].tags.join(", ")}</p>
-            )}
+            <div className="session-subtitle">
+              <div>
+                <strong>{sessions[sessions.length - 1].header}</strong>
+              </div>{" "}
+              {sessions[sessions.length - 1].tags && (
+                <p className="last-session-tags">
+                  <strong>Tags:</strong>{" "}
+                  {sessions[sessions.length - 1].tags.join(", ")}
+                </p>
+              )}
+            </div>
+            <p className="body-text">{sessions[sessions.length - 1].body}</p>
           </div>
         )}
       </div>
