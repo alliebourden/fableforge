94793aa2128b884cd5a9061f751639b51db34773
alliebourden <127435154+alliebourden@users.noreply.github.com>
2024-02-03 18:15:22 -0330

    feat - npcGeneratorChat implemented

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,1 +1,11 @@
-export default function Dashboard() {}
+import generateNPCchat from "../components/npcGeneratorChat";
+
+const getNPCchat = async () => {
+  const response = await generateNPCchat();
+  console.log(response);
+};
+
+export default function Dashboard() {
+  console.log(`This is my open ai key ${import.meta.env.VITE_OPENAI_API_KEY}`);
+  getNPCchat();
+}
