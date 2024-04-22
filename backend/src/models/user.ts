552aa9fbf2468a6c7f837ab6977665bf64dbf781
alliebourden <127435154+alliebourden@users.noreply.github.com>
2024-04-22 13:11:07 -0230

    chore - update user interface / schema

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -3,15 +3,21 @@ import Joi from 'joi'
 type email = string
 
 export interface User {
-  id?: number
+  user_id?: number
   name: string
   email: email
+  username: string
+  password_hash: string
+  salt: string
 }
 
 const UserSchema = Joi.object<User>({
-  id: Joi.number().optional(),
+  user_id: Joi.number().optional(),
   name: Joi.string().required(),
-  email: Joi.string().email().required()
+  email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password_hash: Joi.string().required(),
+  salt: Joi.string().required(),
 })
 
 export const validateUser = (user: any): User | null => {
--- backend/src/utils/Login.jsx
+++ backend/src/utils/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-
+            
         }
     }
 }
