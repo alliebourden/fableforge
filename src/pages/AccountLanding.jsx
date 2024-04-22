a33c73e2705ef39b1a5458dcec927fcce54a5557
alliebourden <127435154+alliebourden@users.noreply.github.com>
2024-04-22 17:36:50 -0230

    build - created landing for account creation/login

---
+++ src/pages/AccountLanding.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { Button, ThemeProvider } from "@mui/material";
+import theme from "../Theme";
+import Login from "../components/Login";
+import CreateAccount from "../components/CreateAccount";
+
+
+export default function AccountLanding() {
+
+  return (
+    <ThemeProvider theme={theme}>
+        <div className="account-landing">
+          <div>
+            <Login />
+          </div>
+          <div>
+            <CreateAccount />
+          </div>
+        </div>
+    </ThemeProvider>
+  );
+}
