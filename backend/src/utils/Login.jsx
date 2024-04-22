ec8df952286202bdd45293914b0990231f52f7c8
alliebourden <127435154+alliebourden@users.noreply.github.com>
2024-04-22 12:47:14 -0230

    chore - create login component file

---
+++ backend/src/utils/Login.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+
+        }
+    }
+}
